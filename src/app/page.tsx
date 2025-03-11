18a6d5b35674abc158e472303e80eeaa823c3813
GustavoPerez <gustavo.perez.231191@gmail.com>
2025-03-11 20:40:30 +0100

    Restore project

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex h-screen items-center justify-center">
-      <h1 className="text-5xl font-bold text-blue-600">Welcome to My Portfolio</h1>
-    </main>
-  );
-}
